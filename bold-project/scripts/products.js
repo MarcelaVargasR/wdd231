8e01b31d33294e3b60d853e038be98a62bab84eb
Marce <lmvt.07@gmail.com>
2024-07-17 17:52:30 -0600

    adding trending page

--- bold-project/scripts/products.js
+++ bold-project/scripts/products.js
@@ -1,5 +1,5 @@
 async function getProducts(){
-    const response = await fetch("#");
+    const response = await fetch("https://raw.githubusercontent.com/MarcelaVargasR/wdd231/main/bold-project/data/products.json");
     const products = await response.json();
     return products;
 }
@@ -7,4 +7,25 @@ async function getProducts(){
 async function renderProducts(){
     const productsContainer = document.getElementById("products");
     const productsData = await getProducts();
+    const productsHtml = productsData.map((product)=>{
+        return `
+            <div class="card-product">
+                <div>
+                    <img
+                    class="img-product"
+                    src="${product.image_url}"
+                    alt="img-1"
+                    />
+                </div>
+                <span class="title-product">${product.name}</span>
+                <p class="product-sub">${product.description}</p>
+                <p class="price">$${product.price}</p>
+            </div>
+        `
+    }).join("")
+
+    productsContainer.innerHTML= productsHtml
 }
+
+
+renderProducts();
